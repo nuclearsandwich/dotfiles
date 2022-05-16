bd964944d432910abaa06cbafb37df80c922fb19
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-05-16 07:44:23 -0700

    Add manual lock command and update lock screen color.
    The black is hard to see in the summer.

--- config/bard/scripts/net_format.sh
+++
@@ -1,8 +0,0 @@
-net_name=$(echo $1 | xargs)
-if [ "$net_name" = "Wired" ]; then
-	echo -en "%{T\$font[icon]}\uf0ac"
-elif [ -z "$net_name" ]; then
-	echo -en "%{F\$color[red]}%{T\$font[icon]}\uf06a %{T-}No Connection"
-else
-	echo -en "%{T\$font[icon]}\uf1eb %{T-}$net_name"
-fi
